6fe9c0254075214e59bae0ed81ae5467be07d80a
orkachlon <or.kachlo@gmail.com>
2023-02-21 14:17:29 +0200

    moved editor script to editor directory first working build!

---
+++ Assets/Scripts/Editor/GraphBuilderEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    [CustomEditor(typeof(GraphBuilder))]
+    [CanEditMultipleObjects]
+    public class GraphBuilderEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            var graphBuilder = target as GraphBuilder;
+            if (!graphBuilder) {
+                return;
+            }
+
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Build Graph")) {
+                graphBuilder.BuildGraph();
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+
+            if (GUILayout.Button("Delete Graph")) {
+                graphBuilder.DeleteGraph();
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+        
+            if (GUILayout.Button("Rebuild Graph")) {
+                graphBuilder.DeleteGraph();
+                graphBuilder.BuildGraph();
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+        }
+    }
+}
